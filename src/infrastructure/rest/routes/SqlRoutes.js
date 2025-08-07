2abb0596df2ea1c4deebce51caa658b06a002bf8
Douglas Farid Cortes Garcia <douglasfarid@gmail.com>
2025-08-07 06:13:25 -0700

    Fix case sensitivity in sqlController import path

--- src/infrastructure/rest/routes/SqlRoutes.js
+++ src/infrastructure/rest/routes/SqlRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const sqlController = require('../controllers/sqlController');
+const sqlController = require('../controllers/SqlController');
 
 router.post('/insert', sqlController.insert);
 router.put('/update', sqlController.update);
